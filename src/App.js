f2e679a3be960930c00cb189b0d757f30e7779b6
kyagie <kyagieboy99@gmail.com>
2022-09-08 12:43:19 +0300

    CSS

--- src/App.js
+++ src/App.js
@@ -2,19 +2,66 @@ import * as React from "react";
 import "./App.css";
 import {
   FormControl,
-  MenuItem,
-  InputLabel,
   Button,
-  Select,
   TextField,
   Box,
-  Grid,
+  InputLabel,
+  Select,
+  MenuItem,
 } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
+import { countryCodeData } from "./data/countryCodeData";
 
 function App() {
   const [countryCode, setCountryCode] = React.useState("");
   const [phoneNumber, setPhoneNumber] = React.useState("");
+  const [errorMessages, setErrorMessages] = React.useState({});
+
+  const handleChange = (event) => {
+    //Remove zero as leading character
+    setPhoneNumber(event.target.value.replace(/^0+/, ""));
+  };
+
+  //On Submit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //Check if validation passes
+    if (handleValidation()) {
+      console.log(countryCode, phoneNumber);
+    }
+  };
+
+  //Handle Validation
+  const handleValidation = () => {
+    let isValid = false;
+
+    if (countryCode && phoneNumber) {
+      isValid = true;
+    } else {
+      setErrorMessages({
+        countryCode: !countryCode ? "Country Code is required" : "",
+        phoneNumber: !phoneNumber ? "Phone Number is required" : "",
+      });
+    }
+
+    //Check if phone number is valid
+    if (phoneNumber) {
+      const regex = new RegExp(/^[0-9\b]+$/);
+      if (!regex.test(phoneNumber)) {
+        setErrorMessages({
+          phoneNumber: "Phone Number is invalid",
+        });
+        isValid = false;
+      } else {
+        setErrorMessages({
+          phoneNumber: "",
+        });
+        isValid = true;
+      }
+    }
+    return isValid;
+  };
 
   return (
     <div className="App">
@@ -29,31 +76,51 @@ function App() {
         <p className="Bold">Start Chatting with</p>
         <div>
           <Box sx={{ flexGrow: 1 }}>
-            <FormControl variant="standard">
-              <Grid container spacing={2}>
-                <Grid xs={6}>
-                  <>
-                    <InputLabel id="country-code-label">
-                      Country Code
-                    </InputLabel>
-                    <Select
-                      labelId="country-code-label"
-                      id="country-code"
-                      value={countryCode}
-                      onChange={(event) => setCountryCode(event.target.value)}
-                    >
-                      <MenuItem value={30}>Thirty</MenuItem>
-                    </Select>
-                  </>
-                </Grid>
-                <Grid xs={6}>
-                  <TextField
-                    id="standard-basic"
-                    label="Standard"
-                    variant="standard"
-                  />
-                </Grid>
-              </Grid>
+            <FormControl
+              variant="standard"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <>
+                <InputLabel id="country-code-label">Code</InputLabel>
+                <Select
+                  labelId="country-code-label"
+                  id="country-code-select"
+                  value={countryCode}
+                  label="Code"
+                  sx={{
+                    width: 100,
+                  }}
+                  onChange={(e) => setCountryCode(e.target.value)}
+                  error={errorMessages.countryCode ? true : false}
+                >
+                  {/* Map Country codes to MenuItem  */}
+                  {countryCodeData.map((item) => (
+                    <MenuItem key={item.code} value={item.dial_code}>
+                      {item.flag} {item.dial_code}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </>
+              <TextField
+                id="telephone"
+                label=" "
+                placeholder="Phone Number"
+                variant="standard"
+                fullWidth
+                error={errorMessages.phoneNumber ? true : false}
+                helperText={errorMessages.phoneNumber}
+                value={phoneNumber}
+                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                onChange={handleChange}
+                sx={{
+                  width: 180,
+                  marginLeft: 2,
+                }}
+              />
             </FormControl>
           </Box>
         </div>
@@ -62,7 +129,12 @@ function App() {
             marginTop: 20,
           }}
         >
-          <Button variant="contained" disableElevation endIcon={<SendIcon />}>
+          <Button
+            variant="contained"
+            onClick={handleSubmit}
+            disableElevation
+            endIcon={<SendIcon />}
+          >
             Go To WhatsApp
           </Button>
         </div>
