6d913ddcdae26912c956b36187c029821bade4b3
kyagie <kyagieboy99@gmail.com>
2022-09-08 16:59:02 +0300

    Fix for phone field

--- src/App.js
+++ src/App.js
@@ -1,127 +1,62 @@
 import * as React from "react";
 import "./App.css";
-import {
-  FormControl,
-  Button,
-  TextField,
-  Box,
-  InputLabel,
-  Select,
-  MenuItem,
-} from "@mui/material";
+import { Button, Box } from "@mui/material";
 import SendIcon from "@mui/icons-material/Send";
-import { countryCodeData } from "./data/countryCodeData";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 function App() {
-  const [countryCode, setCountryCode] = React.useState("");
   const [phoneNumber, setPhoneNumber] = React.useState("");
-  const [errorMessages, setErrorMessages] = React.useState({});
-
-  const handleChange = (event) => {
-    //Remove zero as leading character
-    setPhoneNumber(event.target.value.replace(/^0+/, ""));
-  };
 
   //On Submit
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    //Check if validation passes
-    if (handleValidation()) {
-      console.log(countryCode, phoneNumber);
-    }
-  };
-
-  //Handle Validation
-  const handleValidation = () => {
-    let isValid = false;
-
-    if (countryCode && phoneNumber) {
-      isValid = true;
-    } else {
-      setErrorMessages({
-        countryCode: !countryCode ? "Country Code is required" : "",
-        phoneNumber: !phoneNumber ? "Phone Number is required" : "",
-      });
-    }
-
-    //Check if phone number is valid
-    if (phoneNumber) {
-      const regex = new RegExp(/^[0-9\b]+$/);
-      if (!regex.test(phoneNumber)) {
-        setErrorMessages({
-          phoneNumber: "Phone Number is invalid",
-        });
-        isValid = false;
-      } else {
-        setErrorMessages({
-          phoneNumber: "",
-        });
-        isValid = true;
-      }
-    }
-    return isValid;
+    window.open(`https://wa.me/${phoneNumber}`, "_blank");
   };
 
   return (
     <div className="App">
       <div className="Column">
-        <p className="Bold">WhatsApp Fastlink</p>
+        <p className="Bold">
+          <span
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <WhatsAppIcon />
+          </span>
+          WhatsApp Fastlink
+        </p>
         <p className="Title">
-          Start a WhatsApp conversation before you can save a contact
+          📲 Initiate a WhatsApp conversation before you can save a contact.
         </p>
       </div>
 
       <div className="Middle">
-        <p className="Bold">Start Chatting with</p>
+        <p className="Bold">Start Chatting 💬 with</p>
         <div>
           <Box sx={{ flexGrow: 1 }}>
-            <FormControl
-              variant="standard"
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
+            <PhoneInput
+              country={"ug"}
+              value={phoneNumber}
+              onChange={(phone) => setPhoneNumber(phone)}
+              enableSearch={true}
+              inputStyle={{
+                fontFamily: "Arvo",
+                fontSize: "1.2rem",
+                height: "3.5rem",
               }}
-            >
-              <>
-                <InputLabel id="country-code-label">Code</InputLabel>
-                <Select
-                  labelId="country-code-label"
-                  id="country-code-select"
-                  value={countryCode}
-                  label="Code"
-                  sx={{
-                    width: 100,
-                  }}
-                  onChange={(e) => setCountryCode(e.target.value)}
-                  error={errorMessages.countryCode ? true : false}
-                >
-                  {/* Map Country codes to MenuItem  */}
-                  {countryCodeData.map((item) => (
-                    <MenuItem key={item.code} value={item.dial_code}>
-                      {item.flag} {item.dial_code}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </>
-              <TextField
-                id="telephone"
-                label=" "
-                placeholder="Phone Number"
-                variant="standard"
-                fullWidth
-                error={errorMessages.phoneNumber ? true : false}
-                helperText={errorMessages.phoneNumber}
-                value={phoneNumber}
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
-                onChange={handleChange}
-                sx={{
-                  width: 180,
-                  marginLeft: 2,
-                }}
-              />
-            </FormControl>
+              dropdownStyle={{
+                color: "black",
+                fontFamily: "Arvo",
+              }}
+              searchStyle={{
+                fontFamily: "Arvo",
+              }}
+            />
+            <p>💡 Include the Country Code.</p>
           </Box>
         </div>
         <div
@@ -130,14 +65,41 @@ function App() {
           }}
         >
           <Button
+            id="submit"
             variant="contained"
             onClick={handleSubmit}
             disableElevation
+            disabled={phoneNumber.length < 10}
             endIcon={<SendIcon />}
           >
-            Go To WhatsApp
+            Open Chat
           </Button>
         </div>
+      </div>
+
+      {/* Footer */}
+      <div className="Footer">
+        <p>
+          Made with ❤️ by{" "}
+          <a
+            href="https://twitter.com/kyagie_"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @kyagie_
+          </a>{" "}
+          🇺🇬
+        </p>
+        <p>
+          It's Open Source!{" "}
+          <a
+            href="https://github.com/kyagie/whatsapp-chat-fastlink"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View on GitHub
+          </a>
+        </p>
       </div>
     </div>
   );
